0b7eca28410aa1eac004483f4144595f5020e7c6
KhantYadanarMoe <khantyadanarmoe113@gmail.com>
2025-11-21 10:55:12 +0630

    Blog Scroll to top

--- resources/js/Components/Blog/BlogDetails.jsx
+++ resources/js/Components/Blog/BlogDetails.jsx
@@ -30,6 +30,8 @@ export default function BlogDetails() {
             }
         };
 
+        window.scrollTo({ top: 0, behavior: "smooth" });
+
         getDetails();
         incrementView();
     }, [id]);
