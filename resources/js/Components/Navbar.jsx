9715e1e6b2e112d219d4bf8147e7766f65ba6701
KhantYadanarMoe <khantyadanarmoe113@gmail.com>
2025-12-04 17:03:11 +0630

    Add Admin tab

--- resources/js/Components/Navbar.jsx
+++ resources/js/Components/Navbar.jsx
@@ -75,6 +75,16 @@ export default function Navbar() {
                                     <span>Blogs</span>
                                 </Link>
                             </li>
+                            {user?.isAdmin == 1 && (
+                                <li class="flex items-center justify-center">
+                                    <Link
+                                        to="/admin"
+                                        class="relative text-black cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-black before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-black after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]"
+                                    >
+                                        <span>Admin</span>
+                                    </Link>
+                                </li>
+                            )}
                         </ul>
 
                         <div className="hidden md:flex items-center justify-center gap-4">
@@ -199,6 +209,16 @@ export default function Navbar() {
                                     Blogs
                                 </Link>
                             </li>
+                            {user?.isAdmin == 1 && (
+                                <li>
+                                    <Link
+                                        to="/admin"
+                                        className="hover:text-gray-950"
+                                    >
+                                        Admin
+                                    </Link>
+                                </li>
+                            )}
                         </ul>
                     </div>
                 </nav>
